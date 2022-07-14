a09572347046588451435d0b4c082018a0db07db
Juan Sebastian <jsfc2199@gmail.com>
2022-07-13 22:11:49 -0500

    proyecto inicializado de nuevo usando fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Pokemon from './components/Pokemon'
-import PokemonList from './components/PokemonList'
+
 
 function App() {
 
   return (
     <div className="">
-      Hola desde app xd
-      <PokemonList/>
+ 
     </div>
   )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
-import pokemonReducer from './state/pokemonSlice'
+
 
 
 const store = configureStore ({
     reducer:{
-        pokemons: pokemonReducer,
+
     }
 })
 
